a7d86ae464ba70c2177725f6a3e481e9267c99c8
Gergo D. Nagy <dngege@gmail.com>
2017-10-11 11:53:59 +0200

    replace static functions, refactor

--- I18n.js
+++ I18n.js
@@ -1,33 +1,39 @@
 export default class I18n
 {
     /**
+     * Initialize a new translation instance.
+     */
+    constructor(translations = {})
+    {
+        this.translations = translations;
+    }
+
+    /**
      * Get and replace the string of the given key.
      */
-    static trans(key, replace = {}) 
+    trans(key, replace = {})
     {
-        let translation = this.extract(key, window.translations);
-
-        return this.replace(translation, replace);
+        return this._replace(this._extract(key), replace);
     }
 
     /**
      * Get and pluralize the strings of the given key.
      */
-    static trans_choice(key, count = 1, replace = {})
+    trans_choice(key, count = 1, replace = {})
     {
-        let translation = this.extract(key, window.translations, '|').split('|');
+        let translation = this._extract(key, '|').split('|');
 
         translation = count > 1 ? translation[1] : translation[0];
 
-        return this.replace(translation, replace);
+        return this._replace(translation, replace);
     }
 
     /**
      * Replace the placeholders.
      */
-    static replace(translation, replace)
+    _replace(translation, replace)
     {
-        for (var placeholder in replace) {
+        for (let placeholder in replace) {
             translation = translation.replace(`:${placeholder}`, replace[placeholder]);
         }
 
@@ -35,10 +41,10 @@ export default class I18n
     }
 
     /**
-     * Extract the value from an object by the given key.
+     * The extract helper.
      */
-    static extract(key, object, def = null)
+    _extract(key, value = null)
     {
-        return key.split('.').reduce((t, i) => t[i] || def, object);
+        return key.toString().split('.').reduce((t, i) => t[i] || (value || key), this.translations);
     }
 }
