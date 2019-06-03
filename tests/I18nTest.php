9ce08b3fc65081a8808810a54217902824213bb9
Gege <dngege@gmail.com>
2019-06-03 07:45:55 +0200

    support multiple locales

--- tests/I18nTest.php
+++ tests/I18nTest.php
@@ -2,6 +2,8 @@
 
 namespace Pine\I18n\Test;
 
+use Illuminate\Support\Facades\App;
+
 class I18nTest extends TestCase
 {
     /** @test */
@@ -14,5 +16,15 @@ class I18nTest extends TestCase
     public function translations_can_have_custom_key()
     {
         $this->get('/i18n/custom-key')->assertSee('window.custom = ');
+    }
+
+    /** @test */
+    public function translations_can_be_multilang()
+    {
+        App::setLocale('hu');
+        $this->get('/i18n/translations')->assertSee('"i18n::":{"messages":{"test":"Teszt"}');
+
+        App::setLocale('en');
+        $this->get('/i18n/translations')->assertSee('"i18n::":{"messages":{"test":"Test"}');
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -13,11 +13,16 @@ abstract class TestCase extends BaseTestCase
     {
         parent::setUp();
 
-        View::addNamespace('i18n', __DIR__. '/views');
+        View::addNamespace('i18n', __DIR__ . '/views');
 
         Route::get('/i18n/{view}', function ($view) {
             return view("i18n::{$view}");
         });
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['translator']->addNamespace(__DIR__ . '/lang', 'i18n');
     }
 
     protected function getPackageProviders($app)
