dda24c18dd0b4c69c12eca711c5c55360191e17a
Jesko Karwasz <jesko.karwasz@portavice.de>
2023-08-15 12:44:50 +0200

    Update tests

--- tests/I18nTest.php
+++ tests/I18nTest.php
@@ -9,13 +9,16 @@ class I18nTest extends TestCase
     /** @test */
     public function translations_can_be_printed_via_blade_directive()
     {
-        $this->get('/i18n/translations')->assertSee(json_encode(trans('auth')), false);
+        $this->get('/i18n/translations')
+            ->assertSee(json_encode(trans('auth')), false)
+            ->assertSee('"This is a JSON test":"This is a JSON test"', false);
     }
 
     /** @test */
     public function translations_can_have_custom_key()
     {
-        $this->get('/i18n/custom-key')->assertSee("window['custom'] = ", false);
+        $this->get('/i18n/custom-key')
+            ->assertSee("window['custom'] = ", false);
     }
 
     /** @test */
@@ -24,11 +27,13 @@ class I18nTest extends TestCase
         App::setLocale('hu');
         $this->get('/i18n/translations')
             ->assertSee(json_encode(trans('auth')), false)
+            ->assertSee('"This is a JSON test":"Ez egy JSON teszt"', false)
             ->assertSee('"i18n::":{"messages":{"test":"Teszt"}', false);
 
         App::setLocale('en');
         $this->get('/i18n/translations')
             ->assertSee(json_encode(trans('auth')), false)
+            ->assertSee('"This is a JSON test":"This is a JSON test"', false)
             ->assertSee('"i18n::":{"messages":{"test":"Test"}', false);
     }
 
@@ -38,6 +43,7 @@ class I18nTest extends TestCase
         App::setLocale('fr');
         $this->get('/i18n/translations')
             ->assertSee(json_encode(trans('auth')), false)
+            ->assertSee('"This is a JSON test":"This is a JSON test"', false)
             ->assertSee('"i18n::":{"messages":{"test":"Test"}', false);
     }
 }
